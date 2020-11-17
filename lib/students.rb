4127d20ec6504de2a6d123c0615296b74dc6f723
Plimon Howard <plimon.howard@gmail.com>
2020-11-17 21:02:50 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,13 +1,14 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  medium = "SELECT AVG(gpa) FROM students;"
+
 end
 
 def lowest_student_gpa
 end
 
 def average_student_gpa
+    average = "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
